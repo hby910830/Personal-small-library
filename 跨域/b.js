679f304c7439ddc33e58970bfe78699a32cfd018
hanbaoyi <6045942331@qq.com>
2016-06-06 18:04:21 +0800

    lazy-load

---
+++ 跨域/b.js
@@ -0,0 +1,15 @@
+
+myData({
+	name:'luyoyo',
+    age:'18',
+    sex:'女',
+	place:'上海'
+});
+
+
+//  CORS
+
+// <?php
+// header("Access-Control-Allow-Origin:http://a.com");
+// $myData = '{"name": "luyoyo", "age": 18}';
+// echo $myData; -->
